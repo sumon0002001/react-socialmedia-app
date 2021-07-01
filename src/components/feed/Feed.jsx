c6398f13a7fd9e371ae51635606fbcd3104414e9
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-07-02 03:30:55 +0600

    Add sidebar

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,9 @@
+import "./feed.css";
+
+export default function Feed() {
+    return (
+        <div className="feed">
+            this is feed 
+        </div>
+    )
+}
---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,10 @@
+import "./rightbar.css";
+
+export default function Rightbar() {
+    return (
+        <div className="rightbar">
+        this is rightbar
+            
+        </div>
+    )
+}
